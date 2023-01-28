c40aca9c067d7c5835a5fd5ef7a46e422d1ced6a
kztasa <110252146+kztasa@users.noreply.github.com>
2023-01-28 13:41:25 +0900

    Add files via upload

---
+++ 素数判定.cpp
@@ -0,0 +1,44 @@
+bool isp(ll num)
+{
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+
+    double sqrtNum = sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2)
+    {
+        if (num % i == 0)
+        {
+            // 素数ではない
+            return false;
+        }
+    }
+
+    // 素数である
+    return true;
+}
+
+// エラトステネスの篩を使った素数テーブル
+
+// 1 以上 N 以下の整数が素数かどうかを返す
+vector<bool> Era(ll N) {
+    // テーブル
+    vector<bool> isprime(N + 1, true);
+
+    // 0, 1 は予めふるい落としておく
+    isprime[0] = isprime[1] = false;
+
+    // ふるい
+    for (ll p = 2; p <= N; ++p) {
+        // すでに合成数であるものはスキップする
+        if (!isprime[p]) continue;
+
+        // p 以外の p の倍数から素数ラベルを剥奪
+        for (ll q = p * 2; q <= N; q += p) {
+            isprime[q] = false;
+        }
+    }
+
+    // 1 以上 N 以下の整数が素数かどうか
+    return isprime;
+}
