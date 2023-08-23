cd33f51db46f9945ce8c73aa59777bc08605d92b
kztasa <110252146+kztasa@users.noreply.github.com>
2023-08-23 13:33:14 +0900

    Template.cpp

--- Atcoder _Template.cpp
+++ Atcoder _Template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h> 
 #include <atcoder/all>
+
 #define pub push_back
 #define eb emplace_back
 #define mp make_pair
@@ -10,20 +11,31 @@
 #define per(i, b) per2(i, 0, b)
 #define per2(i, a, b) for (ll i = int(b) - 1; i >= int(a); i--)
 #define all(c) (c).begin(), (c).end()
+
 using namespace std;
 using namespace atcoder;
-using ll = long long; using dd = double;
+
+using ll = long long;
+using dd = double;
 using Pll = pair<ll, ll>;
-using    vll = vector<   ll>; using   vdd = vector< dd>;
-using   vvll = vector<  vll>; using  vvdd = vector<vdd>;
-using  vvvll = vector< vvll>; using vvvdd = vector<vvdd>;
+using    vll = vector<   ll>;
+using   vvll = vector<  vll>; 
+using  vvvll = vector< vvll>; 
 using vvvvll = vector<vvvll>;
+using    vdd = vector<   dd>;
+using   vvdd = vector<  vdd>;
+using  vvvdd = vector< vvdd>;
 using  vchar = vector< char>;
 using vvchar = vector<vchar>;
-using mint = modint998244353;
-using mint2 = modint1000000007;
-using  vmint = vector< mint>; using  vmint2 = vector< mint2>;
-using vvmint = vector<vmint>; using vvmint2 = vector<vmint2>;
+using  vbool = vector< bool>;
+using vvbool = vector<vbool>;
+
+using    mint = modint998244353;
+using   mint2 = modint1000000007;
+using   vmint = vector<  mint>;
+using  vvmint = vector< vmint>;
+using  vmint2 = vector< mint2>; 
+using vvmint2 = vector<vmint2>;
 
 constexpr long long INF = (1LL << 60);
 constexpr double EPS = 1e-9;
@@ -50,7 +62,11 @@ bool chmin(T& a, const T& b) {
 }
 
 ll rem(ll A, ll B) {
-	ll t = A % B; if (t < 0) { t += B; } return t;
+	ll t = A % B;
+	if (t < 0) {
+		t += B;
+	}
+	return t;
 }
 
 string zfill(int n, const int width)
@@ -61,7 +77,7 @@ string zfill(int n, const int width)
 }
 
 //1+2+3+…+N
-ll OnetoN_sum(ll N) {
+ll cumlative_sum(ll N) {
 	return (N * (N + 1)) / 2;
 }
 
@@ -85,33 +101,34 @@ int min(int x, ll y) { return min((ll)x, y); }
 int min(ll x, int y) { return min(x, (ll)y); }
 
 template<typename T = int>
-std::map<T, ll> factorize(T n) {
-	std::map<T, ll> factor_map;
+map<T, ll> factorize(T n) {
+	map<T, ll> factor_map;
 	for (T i = 2; i * i <= n; i++) {
 		while (!(n % i)) {
 			factor_map[i]++; n /= i;
 		}
 	}
-	if (n > 1) factor_map[n]++;
+	if (n > 1) {
+		factor_map[n]++;
+	}
 	return factor_map;
 }
 
 template<typename T = int>
-vector<long long> divisor(T n) {
-	vector<long long> ret;
-	for (long long i = 1; i * i <= n; i++) {
+vll divisor(T n) {
+	vll ret;
+	for (ll i = 1; i * i <= n; i++) {
 		if (n % i == 0) {
 			ret.push_back(i);
 			if (i * i != n) ret.push_back(n / i);
 		}
 	}
-	sort(ret.begin(), ret.end()); // 昇順に並べる
+	sort(all(ret)); // 昇順に並べる
 	return ret;
 }
 
-template<typename T = int>
-vector<vector<ll>> div2(ll N) {
-	vector<vector<ll>> isprime(N + 1);
+vvll div2(ll N) {
+	vvll isprime(N + 1);
 	for (ll p = 1; p <= N; ++p) {
 		for (ll q = p; q <= N; q += p) {
 			isprime[q].eb(p);
@@ -122,10 +139,16 @@ vector<vector<ll>> div2(ll N) {
 
 template<typename T = int>
 bool isp(T num) {
-	if (num < 2) return false;
-	else if (num == 2) return true;
-	else if (num % 2 == 0) return false;
-	double sqrtNum = sqrt(num);
+	if (num < 2) {
+		return false;
+	}
+	else if (num == 2) {
+		return true;
+	}
+	else if (num % 2 == 0) {
+		return false;
+	}
+	dd sqrtNum = sqrt(num);
 	for (int i = 3; i <= sqrtNum; i += 2) {
 		if (num % i == 0) {
 			return false;
@@ -135,8 +158,8 @@ bool isp(T num) {
 }
 
 template<typename T = int>
-vector<bool> Era(ll N) {
-	vector<bool> isprime(N + 1, true);
+vbool Era(ll N) {
+	vbool isprime(N + 1, true);
 	isprime[0] = isprime[1] = false;
 	for (ll p = 2; p <= N; ++p) {
 		if (!isprime[p]) continue;
@@ -148,7 +171,7 @@ vector<bool> Era(ll N) {
 }
 
 const int MAX = 510000;
-const int MOD = 998244353;
+const int MOD = 1020202009;
 long long fac[MAX], finv[MAX], inv[MAX];
 
 void COMinit() {
@@ -162,16 +185,16 @@ void COMinit() {
 	}
 }
 
-long long COM(int n, int k) {
+ll COM(ll n, ll k) {
 	if (n < k) return 0;
 	if (n < 0 || k < 0) return 0;
 	return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
-ll LIS(const std::vector<ll>& v) {
-	std::vector<ll> dp;
+ll LIS(const vll& v) {
+	vector<ll> dp;
 	for (const auto& elem : v) {
-		auto it = std::lower_bound(dp.begin(), dp.end(), elem);
+		auto it = lower_bound(all(dp), elem);
 		if (it == dp.end()) {
 			dp.push_back(elem);
 		}
@@ -240,12 +263,84 @@ vvll BFS2(vvll G) {
 	}
 	return dist;
 }
-
 /////////////////////////////////////////////////////
 
-int main() {
-	cout << fixed << setprecision(10);
+char SMALL(char a) {
+	if (a - 'a' < 26 && a - 'a' >= 0) {
+		return a;
+	}
+	else {
+		return char(a + 32);
+	}
+}
+char LARGE(char a) {
+	if (a - 'A' < 26) {
+		return a;
+	}
+	else {
+		return char(a - 32);
+	}
+}
+
+vll topological_sort(vvll G) {
+	ll n = G.size();
+	vll indeg(n);
+	for (int i = 0; i < n; i++) {
+		for (auto j : G[i]) {
+			indeg[j]++;
+		}
+	}
+	vll res;
+	queue<ll> que;
+	for (int i = 0; i < n; i++) {
+		if (indeg[i] == 0) {
+			que.push(i);
+		}
+	}
+	while (!que.empty()) {
+		ll v = que.front();
+		que.pop();
+		res.push_back(v);
+		for (auto nv : G[v]) {
+			indeg[nv]--;
+			if (indeg[nv] == 0) {
+				que.push(nv);
+			}
+		}
+	}
+	return res;
+}
+
+//N未満でNと互いに素な数の個数
+vll totient(int n) {
+	vbool prime(n + 1, true);
+	vll ans(n + 1);
+	for (int i = 2; i <= n; i++) {
+		ans[i] = i;
+	}
+	if (n >= 0) prime[0] = false;
+	if (n >= 1) prime[1] = false;
+	for (int i = 2; i <= n; i++) {
+		if (!prime[i]) {
+			continue;
+		}
+		ans[i] *= i - 1;
+		ans[i] /= i;
+		for (int j = i + i; j <= n; j += i) {
+			prime[j] = false;
+			ans[j] *= i - 1;
+			ans[j] /= i;
+		}
+	}
+	return ans;
+}
+
+int main(void)
+{
+	//cout << fixed << setprecision(10);
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	
 }
+
